eaacdfb15fb040b59db89ae8745fa75e40092c90
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2021-04-30 15:08:12 +0600

    package install in qr_blade

--- resources/views/qr_code.blade.php
+++ resources/views/qr_code.blade.php
@@ -8,7 +8,7 @@
 
 <div class="text-center" style="margin-top: 50px;">
     <h3>Laravel QR Code Example</h3>
-
+<!-- composer require simplesoftwareio/simple-qrcode (package install)-->
     {!! QrCode::size(300)->generate(Auth::id(). "water" . 30); !!}
     <p>bokhtiar</p>
     <p>MyNotePaper</p>
